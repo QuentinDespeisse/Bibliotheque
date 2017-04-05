274e6d5392aea63ee3cd5d5d3131ee822a48a7d9
QuentinDespeisse <quentin.despeisse@etu.univ-amu.fr>
2017-04-05 10:24:17 +0200

    Creating class Livre

---
+++ src/main/java/mainBiblio/Livre.java
@@ -0,0 +1,57 @@
+package mainBiblio;
+
+public class Livre {
+	
+	private String nom, genre, auteur;
+	private boolean dispo;
+	
+
+	public Livre(String nom, String genre, String auteur) {
+		this.nom = nom;
+		this.genre = genre;
+		this.auteur = auteur;
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public String getGenre() {
+		return genre;
+	}
+
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+
+	public String getAuteur() {
+		return auteur;
+	}
+
+
+	public void setAuteur(String auteur) {
+		this.auteur = auteur;
+	}
+
+
+	public boolean isDispo() {
+		return dispo;
+	}
+
+
+	public void setDispo(boolean dispo) {
+		this.dispo = dispo;
+	}
+	
+	
+
+}
