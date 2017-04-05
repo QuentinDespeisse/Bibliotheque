7a61e67acf819ffa1a372483a24fdbf5859f5f78
Yacine Battis <nalfein84@gmail.com>
2017-04-05 11:12:51 +0200

    Update Bibliotheque.java

--- src/main/java/mainBiblio/Bibliotheque.java
+++ src/main/java/mainBiblio/Bibliotheque.java
@@ -1,10 +1,86 @@
 package mainBiblio;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Bibliotheque {
 
-	public Bibliotheque() {
-		// TODO Auto-generated constructor stub
+	private static Bibliotheque instance = new Bibliotheque();
+	private List<Livre> listLivre;
+	private List<String> listSuggestion;
+	private Map<Client, String> reservation = new HashMap<Client, String>();
+	private Map<Client, String> emprunt = new HashMap<Client, String>();
+	
+	private Bibliotheque() {
+		listLivre = new ArrayList<Livre>();
+		listSuggestion = new ArrayList<String>();
 	}
+	
+	public static Bibliotheque getInstance() {
+		return instance;
+	}
+	
+	public List<Livre> getLivreGenre(String genre) {
+		List<Livre> livresGenre = new ArrayList<Livre>();
+		for(Livre livre: listLivre ){
+			if (livre.getGenre().equals(genre)){
+				livresGenre.add(livre);
+			}
+		}
+		return livresGenre;
+	}
+	
+	public List<Livre> getLivreNom(String nom) {
+		List<Livre> livresNom = new ArrayList<Livre>();
+		for(Livre livre: listLivre ){
+			if (livre.getNom().equals(nom)){
+				livresNom.add(livre);
+			}
+		}
+		return livresNom;
+	}
+	
+	public List<Livre> getLivreAuteur(String auteur) {
+		List<Livre> livresAuteur = new ArrayList<Livre>();
+		for(Livre livre: listLivre ){
+			if (livre.getNom().equals(auteur)){
+				livresAuteur.add(livre);
+			}
+		}
+		return livresAuteur;
+	}
+	
+	public void ajouterReservation(String titre, Client client) {
+		reservation.put(client, titre);
 
+	}
+	
+	public boolean ajouterEmprunt(String titre, Client client) {
+		boolean reserve = false;
+		for (Livre livre : getLivreNom(titre)){
+			if(livre.isDispo()){
+				emprunt.put(client, titre);
+				livre.setDispo(false);
+				reserve = true;
+			}
+
+		}
+		return reserve;
+	}
+	
+	public void rendreEmprunt(Client client) {
+		emprunt.remove(client);
+	}
+	
+	public void ajouterSuggestion(String titre) {
+		listSuggestion.add(titre);
+
+	}
+	
+	public void addLivre(Livre livre) {
+		listLivre.add(livre);
+	}
+	
 }
